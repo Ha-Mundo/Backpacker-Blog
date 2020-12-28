4ccf401dfe6b1e50558114f2d5431e16b42fbce4
Ha-Mundo <trauzzu@gmail.com>
2020-12-28 16:53:29 +0100

    os env

--- flaskblog/users/routes.py
+++ flaskblog/users/routes.py
@@ -34,6 +34,7 @@ def login():
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
+            flash('Welcome!', 'success')
             return redirect(next_page) if next_page else redirect(url_for('main.home'))
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
@@ -43,6 +44,7 @@ def login():
 @users.route("/logout")
 def logout():
     logout_user()
+    flash('You Logged out!', 'info')
     return redirect(url_for('main.home'))
 
 
